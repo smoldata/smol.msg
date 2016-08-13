47d4326142bf4eb9de1670e7d59851c9258b415e
Dan Phiffer <dan@phiffer.org>
2016-08-13 13:39:13 -0400

    :wrench: Avoid double-sending SMS

--- include/msg.php
+++ include/msg.php
@@ -92,18 +92,32 @@ function msg_admin_tx($rx_id, $sender, $msg) {
 
 function msg_send_pending() {
 	$db = db_setup();
-	$query = $db->query("
+	$tx_batch = msg_batch_uuid();
+
+	$query = $db->prepare("
+		UPDATE tx
+		SET transmit_batch = ?
+		WHERE transmit_batch IS NULL
+	");
+	$query->execute(array(
+		$tx_batch
+	));
+
+	$query = $db->prepare("
 		SELECT tx.id AS id,
 		       tx.usr_id AS usr_id,
 		       rx.usr_id AS sender_id,
 		       usr.phone AS phone,
 		       tx.msg AS msg
 		FROM tx, rx, usr
-		WHERE tx.transmitted IS NULL
+		WHERE tx.transmit_batch = ?
 		  AND tx.rx_id = rx.id
 		  AND tx.usr_id = usr.id
 		ORDER BY tx.queued
 	");
+	$query->execute(array(
+		$tx_batch
+	));
 	while ($tx = $query->fetchObject()) {
 		msg_send_sms($tx);
 	}
@@ -426,4 +440,31 @@ function msg_add_to_channel($rx_id, $usr_id, $msg) {
 		$msg,
 		$created
 	));
+}
+
+function msg_batch_uuid() {
+	
+	# https://secure.php.net/manual/en/function.uniqid.php#94959
+
+	# 32 bits for "time_low"
+	# 16 bits for "time_mid"
+		# 16 bits for "time_hi_and_version", four most significant bits holds version number 4
+	# 16 bits, 8 bits for "clk_seq_hi_res", 8 bits for "clk_seq_low", two most significant bits holds zero and one for variant DCE1.1
+	# 48 bits for "node"
+
+	$tl1 = mt_rand(0, 0xffff);
+	$tl2 = mt_rand(0, 0xffff);
+
+	$tm = mt_rand(0, 0xffff);
+	$th = mt_rand(0, 0xffff);
+
+	$cs = mt_rand(0, 0x3fff) | 0x8000;
+
+	$nd1 = mt_rand(0, 0xffff);
+	$nd2 = mt_rand(0, 0xffff);
+	$nd3 = mt_rand(0, 0xffff);
+
+	$fmt = "%04x%04x-%04x-%04x-%04x-%04x%04x%04x";
+
+	return sprintf($fmt, $tl1, $tl2, $tm, $th, $cs, $nd1, $nd2, $nd3);
 }
