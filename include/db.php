4670710734a8e39509506b869f22e8bc8d07200a
Dan Phiffer <dan@phiffer.org>
2016-08-16 14:15:43 -0400

    :wrench: Config path update

--- include/db.php
+++ include/db.php
@@ -2,13 +2,13 @@
 
 function db_setup() {
 	global $db;
-	if (! file_exists('config.php')) {
+	if (! file_exists(dirname(__DIR__) . '/config.php')) {
 		die('You forgot config.php!');
 	}
 	if (! empty($db)) {
 		return $db;
 	}
-	include 'config.php';
+	include dirname(__DIR__) . '/config.php';
 	$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 	$db = new PDO($dsn, $db_user, $db_pass);
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
